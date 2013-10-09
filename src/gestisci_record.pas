300b187fd4c0de21f4807327cb5e45a6fdf92b50
Utente <utente@PC-Utente.(none)>
2013-10-09 09:50:35 +0200

    Aggiunto programma gesione record

---
+++ src/gestisci_record.pas
@@ -0,0 +1,33 @@
+Program gestisci_record;
+
+type
+	t_fattura=record
+		numero:integer;
+		data:string[8];
+		impresa:string[20];
+	end;
+
+var 
+	fatture:array[0..100] of t_fattura;
+	n, i:byte;
+
+begin
+	writeln('Quante fatture vuoi inserire? ');
+	readln(n);
+	for i:=1 to n do
+		begin
+			write('Inserisci il numero della ',i , ' fattura: ');
+			readln(fatture[i].numero);
+			write('Inserisci la data: ');
+			readln(fatture[i].data);
+			write('Inserisci l'' impresa: ');
+			readln(fatture[i].impresa);
+		end;
+
+	for i:=1 to n do
+		begin
+			writeln(fatture[i].numero);
+			writeln(fatture[i].data);
+			writeln(fatture[i].impresa);
+		end;
+end.
