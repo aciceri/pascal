ef793bc537346097b330c61c3334e34a6f267c17
Utente <utente@PC-Utente.(none)>
2013-10-24 12:59:53 +0200

    Modificato

--- src/archivio_cognomi.pas
+++ src/archivio_cognomi.pas
@@ -1,6 +1,8 @@
 program archivio_cognomi;
 uses sysutils;
 
+const percorso='cognomi.dat';
+
 type
 	t_cognome=string[30];
 	t_file_cognomi=file of t_cognome;
@@ -9,16 +11,21 @@ var
 	cognome: t_cognome;
 	archivio: t_file_cognomi;
 
-procedure carica(var archivio: t_file_cognomi);
-	var continua: char;
-		i, num: integer;
+procedure inizializza(var archivio: t_file_cognomi);
 begin
-	assign(archivio, 'cognomi.dat');
-	if not FileExists('cognomi.dat') then
-		rewrite(archivio);
+	assign(archivio, percorso);
+	if not FileExists(percorso) 
+		then rewrite(archivio);
+end;
+
+procedure carica(var archivio: t_file_cognomi);
+	var i, num: integer;
+		tmp: t_cognome;
+begin
 	reset(archivio);
 	write('Quanti cognomi vuoi inserire? ');
 	readln(num);
+	while not eof(archivio) do read(archivio, tmp);
 	for i:=1 to num do
 		begin
 		write('Inserisci il ', i,' cognome: ');
@@ -28,11 +35,18 @@ begin
 end;
 
 procedure visualizza(var archivio: t_file_cognomi);
+	var pippo: t_cognome;
 begin
 	reset(archivio);
+	while not eof(archivio) do
+		begin
+		read(archivio, pippo);
+		writeln(pippo);
+		end;
 end;
 
 begin
+	inizializza(archivio);
 	carica(archivio);
 	visualizza(archivio);
 end.
